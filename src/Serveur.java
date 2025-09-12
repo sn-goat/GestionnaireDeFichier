bf7d3eda402273c42c5854befa2636217761e3c6
sn-goat <sergile-junior.domguia-nyouvop@polymtl.ca>
2025-09-11 21:17:39 -0400

    feat(1 et 2): il faut juste les transformer en fonctions pour les mettre dans une class Util n pour que le Client et le Server puissent les utiliser

--- src/Serveur.java
+++ src/Serveur.java
@@ -1,15 +1,69 @@
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
 
 public class Serveur
 {
 	private static ServerSocket listener;
+	
+	private final static String IP_ADDRESS = "127.0.0.1";
+	
+	private static  boolean isValidPort = false;
+	private  static boolean isValidIP = false;
+	
 
 	public static void main(String[] args) throws Exception
 	{
 		int clientNumber = 0;
-		String serverAddress = "127.0.0.1";
-		int serverPort = 5000;
+		Scanner scanner = new Scanner(System.in);
+		String serverAddress = null;
+		int serverPort = 0;
+
+		
+		while(!isValidIP) {
+			System.out.print("Enter a valid IP address: ");
+			serverAddress = scanner.nextLine();
+		
+			String[] bytes = serverAddress.split("\\.");
+			int number = 0;
+			
+			try {
+				if(bytes.length == 4 ) {
+						for(int i = 0; i < 4; ++i) {
+							number = Integer.parseInt(bytes[i]);
+							if (number < 0 || number > 255) {
+								throw new Exception("Each byte must be within 0 and 255 inclusively.");
+							}
+						}
+						isValidIP = true;
+				} else {
+					throw new Exception("The IP address must be only 4 byte long.");
+				}
+			} catch (Exception e ) {
+				System.out.println(e.getMessage());
+				System.out.println("Please enter a valid IP format.");
+				
+			} 
+						
+				
+		}
+		
+		while(!isValidPort) {
+			System.out.print("Enter a valid listenig port number: ");
+			try {
+				serverPort = scanner.nextInt();
+				if(serverPort >= 5000 && serverPort <= 5050) {
+					isValidPort = true;
+				} else {
+					throw new Exception("The port number is not within 5000 and 5050.");
+				}
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				System.out.println("Please enter a valid type.");
+				scanner.nextLine();
+			} 
+		}
+		
 
 		listener = new ServerSocket();
 		listener.setReuseAddress(true);
@@ -17,6 +71,7 @@ public class Serveur
 
 		listener.bind(new InetSocketAddress(serverIP, serverPort));
 		System.out.format("The server is running %s:%d\n", serverAddress, serverPort);
+		scanner.close();
 
 		try
 		{
